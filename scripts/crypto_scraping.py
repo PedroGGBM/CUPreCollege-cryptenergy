f772450332ffa6636a9d27ca9839345e4c775686
Pedro G <47034242+PedroGGBM@users.noreply.github.com>
2021-07-13 17:03:40 +0200

    Update crypto_scraping.py

--- scripts/crypto_scraping.py
+++ scripts/crypto_scraping.py
@@ -10,7 +10,7 @@ market = 'USD'
 
 def get_crypto_raw(API_KEY, function, symbol, market):
 
-    url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&market={market}&apikey={API_KEY}'
+    url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&market={market}&apikey={API_KEY}&datatype=csv'
     response = requests.get(url)
     raw_data = response.text
     data = list(csv.reader(raw_data.splitlines()))
@@ -18,10 +18,10 @@ def get_crypto_raw(API_KEY, function, symbol, market):
     return data
 
 def write_crypto_data(data, symbol):
-    doc = open(f"prices.{symbol}.txt", 'w')
+    doc = open(f"prices.{symbol}.csv", 'w')
     # for loop will be used to write (full outputsize) the date (index position [i][0])
-    for i in range (1, len(data)):
-        doc.write(f"{data[i][0]}\n")
+    for i in range (0, len(data)):
+        doc.write(f"{str(data[i][0:5])} {str(data[i][9:11])}\n")
     doc.close()
     # program output (not doc.write) for the stock symbol written through command line (argv)
     print(f"Wrote historical price data for {symbol} to file price.{symbol}.txt")
