b4150b40eb73b15afe3e2ea824cd5a11f55f1f73
Ethan Wong <84290738+ethanwong05@users.noreply.github.com>
2021-07-14 09:48:23 -0400

    Delete import_data.r

--- scripts/import_data.r
+++
@@ -1,7 +0,0 @@
-energy_data <- read_csv("data/daily_values_ee.csv")
-
-ETH_data <- read_csv("data/prices.ETH.csv")
-
-BTC_data <- read_csv("data/prices.BTC.csv")
-
-inflation_data <- read_csv("data/breakeven_inflation_us.csv")
