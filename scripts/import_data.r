b9881377da449f502f8598a92f7e20683c2b8d11
Ethan Wong <84290738+ethanwong05@users.noreply.github.com>
2021-07-14 09:49:11 -0400

    Add files via upload

---
+++ scripts/import_data.r
@@ -0,0 +1,9 @@
+BTC_energy <- read_csv("data/data-cFnri")
+
+ETH_energy <- read_csv("data/data-ocIBH")
+
+ETH_data <- read_csv("data/prices.ETH.csv")
+
+BTC_data <- read_csv("data/prices.BTC.csv")
+
+inflation_data <- read_csv("data/breakeven_inflation_us.csv")
