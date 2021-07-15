355c3cd387d27da47990bb9a094193327e704df7
Pedro G <47034242+PedroGGBM@users.noreply.github.com>
2021-07-15 19:22:34 +0200

    Update reg_regression.py

--- modeling/reg_regression.py
+++ modeling/reg_regression.py
@@ -1,40 +1,49 @@
-# Regression in Python ----
+# Crypto Regression ----
 
-# Linear ----
-
-# Classification ----
+# Essential 
 import sklearn
 import numpy as np
 import pandas as pd
 import os
+
+# Scikit-learn modules
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn import utils
 from sklearn import preprocessing
 
-os.chdir("C:/Users/wongs/Desktop/final-project")
+# Setting working directory
+os.chdir("C:/Users/Pedro G/OneDrive/Desktop/final_project")
 
-data = pd.read_csv("data/data.csv")
+# Reading csv data 
+data = pd.read_csv("data/preprocessed_data.csv")
 
 data['timestamp'] = pd.to_datetime(data['timestamp'])
 
-breakeven_inflation = data['breakeven_inflation']
-data = data.drop(columns = ['breakeven_inflation', 'timestamp'])
+breakeven_inflation = data['']
+data = data.drop(columns = ['ETH_Energy', 'timestamp'])
 
+# defining target variable and data 
 X = data
 y = breakeven_inflation
-from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 y_test = y_test.astype('int')
 y_train = y_train.astype('int')
 X_train = X_train.astype('int')
 X_test = X_test.astype('int')
 
-from sklearn.linear_model import LogisticRegression
+# Regression model 
 model = LogisticRegression(solver='lbfgs',
 multi_class ='multinomial')
 model.fit(X_train, y_train)
-from sklearn.metrics import confusion_matrix
+
 # Test result
 y_predict = model.predict(X_test)
 confusion_matrix(y_test, y_predict)
-from sklearn.metrics import accuracy_score
-accuracy_score(y_test, y_predict)*100
+
+# Accuracy result
+print(f"\n- accuracy: {accuracy_score(y_test, y_predict)*100:.1f}% -\n")
